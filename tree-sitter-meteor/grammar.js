8c5ea632157c272fe59e53451361d982e9add809
Nechi <nechifechi@gmail.com>
2026-08-20 08:59:16 +0500

    updt

---
+++ tree-sitter-meteor/grammar.js
@@ -0,0 +1,49 @@
+module.exports = grammar({
+    name: "meteor",
+
+    extras: $ => [
+        /\s/,
+        $.comment
+    ],
+
+    rules: {
+        source_file: $ => repeat($._statement),
+
+        _statement: $ => choice(
+            $.write_statement,
+            $.exec_statement
+        ),
+
+        write_statement: $ => seq(
+            "write",
+            "(",
+            $.string,
+            optional(seq(
+                ",",
+                $.boolean
+            )),
+            ")",
+            ";"
+        ),
+
+        exec_statement: $ => seq(
+            "exec",
+            "(",
+            $.string,
+            ")",
+            ";"
+        ),
+
+        string: $ => /"([^"\\]|\\.)*"/,
+
+        boolean: $ => choice(
+            "true",
+            "false"
+        ),
+
+        comment: $ => token(seq(
+            "//",
+            /.*/
+        ))
+    }
+});
---
+++ tree-sitter-meteor/queries/highlights.scm
@@ -0,0 +1,11 @@
+(write_statement
+    "write" @keyword)
+
+(exec_statement
+    "exec" @keyword)
+
+(string) @string
+
+(boolean) @boolean
+
+(comment) @comment
